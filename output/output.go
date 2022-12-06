114a40ea5084dc3befc5b6b3aa5beb8577067d69
Guillermo Vaya <guivaya@gmail.com>
2022-12-06 17:35:28 +0100

    write results

---
+++ output/output.go
@@ -0,0 +1,33 @@
+package output
+
+import (
+	"os"
+
+	"github.com/gocarina/gocsv"
+)
+
+type Match struct {
+	SenderEmail   string
+	SenderName    string
+	ReceiverEmail string // this is for id purposes for next year
+	ReceiverName  string
+	Address1      string
+	Address2      string
+	City          string
+	Zip           string
+	Province      string
+	Country       string
+	Phone         string
+	CreatedAt     string
+}
+
+const OutputRow string = "{.SenderEmail},{.SenderName},{.ReceiverEmail},{.ReceiverName},{.Address1},{.Address2},{.City},{.Zip},{.Province},{.Country},{.Phone},{.CreatedAt}"
+
+func WriteCSV(results []*Match, outputFile string) error {
+	csvContent, err := gocsv.MarshalString(&results)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outputFile, []byte(csvContent), 0666)
+}
